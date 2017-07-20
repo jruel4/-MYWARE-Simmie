0db108a7d9faed209fd763628dd9503c0c19f313
martianmartin <flowt-biohacker@gmail.com>
2017-07-20 12:12:47 -0400

    updated adapters

--- Simmie/InterfaceAdapters/Input_Adapters/EEG_State_Adapter.py
+++ Simmie/InterfaceAdapters/Input_Adapters/EEG_State_Adapter.py
@@ -5,3 +5,76 @@ Description: Formatted EEG data of basic and expert features.
 
 subprocesses: P1 input and P2 input
 '''
+
+'''
+EEG features will just be spectrogram at first.
+
+- 5 time points (10 seconds w/ 2 second epochs)
+- 30 frequencies
+- 16 channels
+'''
+
+from threading import Thread, Event
+from pylsl import StreamInfo, StreamOutlet, StreamInlet, resolve_stream
+
+class EEGStateAdapter:
+    
+    def __init__(self):
+        self.eeg_thread_event = Event()
+        self.eeg_data_cache = list()
+        
+    def retrieve_eeg_state(self):
+        #TODO what numpy format is good?
+        pass
+    
+    def launch_eeg_adapter(self, manual_stream_select=True): 
+        streams = resolve_stream('type', 'AudioCommands')
+        snum = 0
+        if manual_stream_select:
+            for i,s in enumerate(streams):
+                print(i,s.name())
+            snum = input("Select EEGStateAdapter stream: ")
+        self.inlet = StreamInlet(streams[snum])
+        # launch thread
+        self.command_thread_event.set()
+        thread = Thread(target=self.command_rx_thread)
+        thread.start()
+        
+    def stop_eeg_thread(self):
+        self.command_thread_event.clear()
+
+    def eeg_rx_thread(self):
+        '''
+        Receiver will need to select correct stream, then continuously accept and 
+        process commands as they arrive.
+        '''
+        
+        while self.command_thread_event.isSet():
+    
+            # get command
+            command_set, timestamps = self.inlet.pull_sample()
+            
+            # cache
+            self.imprint_data_cache += [(command_set, timestamps)]
+            
+        print("Exiting imprint adapter rx thread")
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- Simmie/InterfaceAdapters/Input_Adapters/Reward_Punish_Adapter.py
+++ Simmie/InterfaceAdapters/Input_Adapters/Reward_Punish_Adapter.py
@@ -1,7 +1,19 @@
 '''
-Source: User via StreamManager
+Source: User via KeyInput LSL App
 
 Description: Positive, negative and neutral feedback from user for training Target network.
 
 subprocesses: P4 input
 '''
+
+'''
+Reward punish data will comm from LSL Key Capture App. 
+
+Assume some format
+'''
+
+
+
+
+
+
